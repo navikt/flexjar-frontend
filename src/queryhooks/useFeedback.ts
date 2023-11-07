69d1e4845c0c0242a038bae72f0d902e2974fcd7
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-07 14:56:14 +0100

    Test tanstacktable (#206)

--- src/queryhooks/useFeedback.ts
+++ src/queryhooks/useFeedback.ts
@@ -5,7 +5,16 @@ import { fetchJsonMedRequestId } from '../utils/fetch'
 export function useFeedback(team: string | string[], refetchEvery30Seconds = false): UseQueryResult<Feedback[], Error> {
     return useQuery<Feedback[], Error>({
         queryKey: [`feedback-${team}`],
-        queryFn: () => fetchJsonMedRequestId(`/api/flexjar-backend/api/v1/intern/feedback?team=${team}`),
+        queryFn: async () => {
+            const fetchet: Feedback[] = await fetchJsonMedRequestId(
+                `/api/flexjar-backend/api/v1/intern/feedback?team=${team}`,
+            )
+            fetchet.sort((a, b) => {
+                return new Date(b.opprettet).getTime() - new Date(a.opprettet).getTime()
+            })
+            return fetchet
+        },
+
         refetchInterval: refetchEvery30Seconds ? 30 * 1000 : undefined,
     })
 }
