e9a9032cdb1a5ee948e38f3b76f6b6f9821cdef9
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-08 13:09:43 +0100

    paginering-api

--- src/testdata/testdata.ts
+++ src/testdata/testdata.ts
@@ -19,12 +19,14 @@ for (let i = 0; i < antallFeedback; i++) {
             feedback: faker.datatype.boolean() ? faker.company.catchPhrase() : '',
             svar: faker.datatype.boolean() ? 'JA' : 'NEI',
             app: faker.color.human(),
+            team: faker.datatype.boolean() ? 'flex' : 'teamsykmelding',
             feedbackId: faker.science.chemicalElement().name,
         },
         id: faker.string.uuid(),
         opprettet: faker.date.past().toISOString(),
     })
 }
+
 function deleteFeedbackById(idToDelete: string): void {
     testdata = testdata.filter((feedback) => feedback.id !== idToDelete)
 }
@@ -42,6 +44,41 @@ export async function mockApi(opts: BackendProxyOpts): Promise<void> {
         res.end()
         return
     }
+
+    if (validert.api == 'GET /api/v1/intern/feedback-pagable') {
+        const page = parseInt(validert.query.get('page') || '0', 10)
+        const size = parseInt(validert.query.get('size') || '10', 10)
+        const team = validert.query.get('team') || 'flex'
+        const medTekst = (validert.query.get('medTekst') || 'false') == 'true'
+
+        // Filter data based on 'feedback' query parameter if it's provided
+        const filteredData = testdata
+            .filter((feedback) => {
+                return feedback.feedback.team === team
+            })
+            .filter((feedback) => {
+                if (medTekst) {
+                    return feedback.feedback.feedback
+                }
+                return true
+            })
+
+        // Implement pagination logic
+        const paginatedData = filteredData.slice(page * size, (page + 1) * size)
+
+        // Create a response object that includes pagination information
+        const response = {
+            content: paginatedData,
+            totalPages: Math.ceil(filteredData.length / size),
+            totalElements: filteredData.length,
+            size: size,
+            number: page,
+        }
+
+        res.status(200).json(response)
+        return
+    }
+
     if (validert.api == 'DELETE /api/v1/intern/feedback/[uuid]') {
         const id = req.url?.split('/').pop()
         deleteFeedbackById(id || '')
