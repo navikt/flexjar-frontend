6ffaa44a915f87659cae9dfd7d91e18c10b99717
oivindkulsrud <oivindkulsrud@gmail.com>
2023-11-22 15:42:06 +0100

    wip

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,28 +1,66 @@
-import { useState } from 'react'
-import { Tag, UNSAFE_Combobox } from '@navikt/ds-react'
+import React from 'react';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { UNSAFE_Combobox } from '@navikt/ds-react';
 
-import { Feedback } from '../queryhooks/useFeedback'
+import { Feedback } from '../queryhooks/useFeedback';
+
+// API interaction functions
+const fetchTags = async (): Promise<string[]> => {
+  const response = await fetch('/api/v1/intern/feedback/tags');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const addTag = async (tag: string): Promise<void> => {
+  await fetch('/api/v1/intern/feedback/:id/tags', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ tag }),
+  });
+};
 
 export const Tags = ({ feedback }: { feedback: Feedback }): JSX.Element => {
-    const [selectedTags, setSelectedTags] = useState<string[]>(feedback.tags)
+  const queryClient = useQueryClient();
 
-    return (
-        <div>
-            <UNSAFE_Combobox
-                allowNewValues
-                label="Hva er dine favorittdrikker? Legg gjerne til flere alternativer."
-                options={feedback.tags}
-                isMultiSelect
-                selectedOptions={selectedTags}
-                onToggleSelected={(option, isSelected) => {
-                    if (isSelected) {
-                        setSelectedTags([...selectedTags, option])
-                    } else {
-                        setSelectedTags(selectedTags.filter((item) => item !== option))
-                    }
-                }
-                }
-            />
-        </div>
-    )
-}
+  // Fetch tags using React Query
+  const { data: tags, isLoading, isError } = useQuery('tags', fetchTags);
+
+  // Mutation for adding a tag
+  const addTagMutation = useMutation(addTag, {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries('tags');
+    },
+  });
+
+  // Handle tag toggle
+  const handleTagToggle = (tag: string, isSelected: boolean): void => {
+    if (isSelected) {
+      addTagMutation.mutate(tag);
+    } else {
+      // Handle tag removal logic
+    }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>An error has occurred</div>;
+
+  return (
+    <div>
+      <UNSAFE_Combobox
+        allowNewValues
+        label="Hva er dine favorittdrikker? Legg gjerne til flere alternativer."
+        options={tags || []} // Use fetched tags
+        isMultiSelect
+        selectedOptions={feedback.tags} // Assuming 'feedback.tags' is an array of selected tags
+        onToggleSelected={(option, isSelected) => {
+          handleTagToggle(option, isSelected);
+        }}
+      />
+    </div>
+  );
+};
