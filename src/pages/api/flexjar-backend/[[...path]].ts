b86190ef95ae2a30938af3b1d1d09fbedd73c6eb
oivindkulsrud <oivindkulsrud@gmail.com>
2023-11-28 09:05:13 +0100

    add some api endpoints

--- src/pages/api/flexjar-backend/[[...path]].ts
+++ src/pages/api/flexjar-backend/[[...path]].ts
@@ -10,6 +10,10 @@ const { serverRuntimeConfig } = getConfig()
 
 const tillatteApier = [
     'GET /api/v1/intern/feedback',
+    'GET /api/v1/intern/tags',
+    'POST /api/v1/intern/feedback/[uuid]/tags',
+    'DELETE /api/v1/intern/feedback/[uuid]/tags',
+    'DELETE /api/v1/intern/feedback',
     'GET /api/v1/intern/feedback-pagable',
     'DELETE /api/v1/intern/feedback/[uuid]',
     'POST /api/v1/feedback/azure',
