c110df471b6c388d36cf7c0b4e6e6694f8568504
oivindkulsrud <oivindkulsrud@gmail.com>
2023-12-05 09:27:57 +0100

    stjerneknapp klar for testing

--- src/components/Stjerneknapp.tsx
+++ src/components/Stjerneknapp.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@navikt/ds-react'
 import React from 'react'
-// import { useQueryClient } from '@tanstack/react-query'
+import { useQueryClient } from '@tanstack/react-query'
 import { StarIcon } from '@navikt/aksel-icons'
 
 import { Feedback } from '../queryhooks/useFeedback'
@@ -8,16 +8,21 @@ import { Feedback } from '../queryhooks/useFeedback'
 import { addTag, deleteTag } from './Tags'
 
 export const Stjerneknapp = ({ feedback }: { feedback: Feedback }): JSX.Element => {
-    // const queryClient = useQueryClient()
     const [erStjerne, setErStjerne] = React.useState<boolean>(feedback.tags.includes('stjerne'))
-    // use state to handle display of changes, only invalidate on errors
+    const queryClient = useQueryClient()
+
     const toggleStjerne = async (feedback: Feedback): Promise<void> => {
-        if (erStjerne) {
-            await deleteTag('stjerne', feedback.id)
-            setErStjerne(false)
-        } else {
-            await addTag('stjerne', feedback.id)
-            setErStjerne(true)
+        try {
+            if (erStjerne) {
+                setErStjerne(false)
+                await deleteTag('stjerne', feedback.id)
+            } else {
+                setErStjerne(true)
+                await addTag('stjerne', feedback.id)
+            }
+        } catch (err) {
+            alert('Det har skjedd en feil, dine siste endringer ble ikke lagret')
+            queryClient.invalidateQueries()
         }
     }
 
@@ -29,7 +34,7 @@ export const Stjerneknapp = ({ feedback }: { feedback: Feedback }): JSX.Element 
             }}
             variant={erStjerne ? 'primary' : 'secondary'}
         >
-            <StarIcon title="a11y-title" fontSize="1.5rem" className={erStjerne ? 'text-white' : ''} />
+            <StarIcon title="a11y-stjerne" fontSize="1.5rem" className={erStjerne ? 'text-white' : ''} />
         </Button>
     )
 }
