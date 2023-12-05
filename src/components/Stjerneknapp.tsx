9773953ac6ed4cd91de8d1d253ff0d81e8457ecd
oivindkulsrud <oivindkulsrud@gmail.com>
2023-12-05 09:49:17 +0100

    bruker react query

--- src/components/Stjerneknapp.tsx
+++ src/components/Stjerneknapp.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@navikt/ds-react'
 import React from 'react'
-import { useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Button } from '@navikt/ds-react'
 import { StarIcon } from '@navikt/aksel-icons'
 
 import { Feedback } from '../queryhooks/useFeedback'
@@ -11,29 +11,27 @@ export const Stjerneknapp = ({ feedback }: { feedback: Feedback }): JSX.Element 
     const [erStjerne, setErStjerne] = React.useState<boolean>(feedback.tags.includes('stjerne'))
     const queryClient = useQueryClient()
 
-    const toggleStjerne = async (feedback: Feedback): Promise<void> => {
-        try {
-            if (erStjerne) {
-                setErStjerne(false)
-                await deleteTag('stjerne', feedback.id)
-            } else {
-                setErStjerne(true)
-                await addTag('stjerne', feedback.id)
-            }
-        } catch (err) {
-            alert('Det har skjedd en feil, dine siste endringer ble ikke lagret')
-            queryClient.invalidateQueries()
-        }
+    const mutation = useMutation(
+        (tag: string) => {
+            return erStjerne ? deleteTag(tag, feedback.id) : addTag(tag, feedback.id)
+        },
+        {
+            onSuccess: () => {
+                setErStjerne(!erStjerne)
+                queryClient.invalidateQueries()
+            },
+            onError: (error) => {
+                alert('Det har skjedd en feil, dine siste endringer ble ikke lagret' + error)
+            },
+        },
+    )
+
+    const toggleStjerne = (): void => {
+        mutation.mutate('stjerne')
     }
 
     return (
-        <Button
-            size="small"
-            onClick={async () => {
-                await toggleStjerne(feedback)
-            }}
-            variant={erStjerne ? 'primary' : 'secondary'}
-        >
+        <Button size="small" onClick={toggleStjerne} variant={erStjerne ? 'primary' : 'secondary'}>
             <StarIcon title="a11y-stjerne" fontSize="1.5rem" className={erStjerne ? 'text-white' : ''} />
         </Button>
     )
