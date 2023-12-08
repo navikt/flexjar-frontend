382ad03163dffc9c01fd1c53693a618d8ee6a249
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-08 13:21:19 +0100

    5 per page også

--- src/components/FeedbackTabell.tsx
+++ src/components/FeedbackTabell.tsx
@@ -297,7 +297,7 @@ export const FeedbackTabell = (): React.JSX.Element | null => {
                         setSize(Number(e.target.value))
                     }}
                 >
-                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                    {[5, 10, 20, 30, 40, 50].map((pageSize) => (
                         <option key={pageSize} value={pageSize}>
                             Show {pageSize}
                         </option>
