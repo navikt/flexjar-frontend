dce31831009be9539b92be6ae6d159f11992fe5d
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-28 10:53:05 +0100

    Ubrukt kode

--- src/queryhooks/useFeedback.ts
+++ src/queryhooks/useFeedback.ts
@@ -1,24 +1,3 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query'
-
-import { fetchJsonMedRequestId } from '../utils/fetch'
-
-export function useFeedback(team: string | string[], refetchEvery30Seconds = false): UseQueryResult<Feedback[], Error> {
-    return useQuery<Feedback[], Error>({
-        queryKey: [`feedback-${team}`],
-        queryFn: async () => {
-            const fetchet: Feedback[] = await fetchJsonMedRequestId(
-                `/api/flexjar-backend/api/v1/intern/feedback?team=${team}`,
-            )
-            fetchet.sort((a, b) => {
-                return new Date(b.opprettet).getTime() - new Date(a.opprettet).getTime()
-            })
-            return fetchet
-        },
-
-        refetchInterval: refetchEvery30Seconds ? 30 * 1000 : undefined,
-    })
-}
-
 export interface Feedback {
     feedback: FeedbackInput
     id: string
