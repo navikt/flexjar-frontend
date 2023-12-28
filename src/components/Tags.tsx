1a7eda1449ec56ef2dcb2aad5514ae6eb8c869ec
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-28 14:18:06 +0100

    Optimistic add tag

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -4,13 +4,14 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { Feedback } from '../queryhooks/useFeedback'
 import { fetchJsonMedRequestId, fetchMedRequestId } from '../utils/fetch'
+import { PageResponse } from '../testdata/testdata'
 
-async function fetchAllTags(): Promise<string[]> {
+async function fetchAllTags(): Promise<Set<string>> {
     const url = `/api/flexjar-backend/api/v1/intern/feedback/tags`
 
     const fetchet: string[] = await fetchJsonMedRequestId(url)
 
-    return fetchet || []
+    return new Set(fetchet)
 }
 
 export async function addTag(tag: string, id: string): Promise<void> {
@@ -22,7 +23,7 @@ export async function addTag(tag: string, id: string): Promise<void> {
         },
         body: JSON.stringify({ tag }),
     }
-    fetchMedRequestId(url, options)
+    await fetchMedRequestId(url, options)
 }
 
 export const deleteTag = async (tag: string, id: string): Promise<void> => {
@@ -40,21 +41,32 @@ export const Tags = ({ feedback }: { feedback: Feedback }): JSX.Element => {
         queryFn: fetchAllTags,
         queryKey: ['allTags'],
     })
-    const allTagsUtenStjerne = allTags?.filter((x) => x !== 'stjerne') || []
+    const allTagsSomArray = Array.from(allTags || [])
+    const allTagsUtenStjerne = allTagsSomArray?.filter((x) => x !== 'stjerne') || []
 
     const addTagMutation = useMutation({
-        onMutate: async () => {
-            //TODO optimistic
+        onMutate: async ({ tag, id }) => {
+            const queriesData = queryClient.getQueriesData<PageResponse>({ queryKey: ['feedback'] })
+            const querydata = queriesData.find((a) => {
+                return a[1]?.content.find((b) => {
+                    if (b.id === id) {
+                        b.tags.push(tag)
+                        return true
+                    }
+                    return false
+                })
+            })
+            if (querydata) {
+                queryClient.setQueryData<PageResponse>(querydata[0], querydata[1])
+            }
+            if (allTags) {
+                allTags?.add(tag)
+                queryClient.setQueryData(['allTags'], allTags)
+            }
         },
         mutationFn: ({ tag, id }: { tag: string; id: string }) => addTag(tag, id),
         onError: () => {
             alert('Det har skjedd en feil, dine siste endringer ble ikke lagret')
-            queryClient.invalidateQueries()
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ['allTags'],
-            })
             queryClient.invalidateQueries()
         },
     })
@@ -68,12 +80,6 @@ export const Tags = ({ feedback }: { feedback: Feedback }): JSX.Element => {
             alert('Det har skjedd en feil, dine siste endringer ble ikke lagret')
             queryClient.invalidateQueries()
         },
-        onSuccess: async () => {
-            await queryClient.invalidateQueries()
-            queryClient.invalidateQueries({
-                queryKey: ['allTags'],
-            })
-        },
     })
 
     const handleTagToggle = (tag: string, isSelected: boolean): void => {
@@ -86,18 +92,16 @@ export const Tags = ({ feedback }: { feedback: Feedback }): JSX.Element => {
 
     if (isErrorAllTags) return <div>Det har skjedd en feil</div>
     return (
-        <div>
-            <UNSAFE_Combobox
-                allowNewValues
-                isMultiSelect
-                label="Tags"
-                hideLabel={true}
-                options={allTagsUtenStjerne}
-                selectedOptions={feedbackTags}
-                onToggleSelected={(option, isSelected) => {
-                    handleTagToggle(option, isSelected)
-                }}
-            />
-        </div>
+        <UNSAFE_Combobox
+            allowNewValues
+            isMultiSelect
+            label="Tags"
+            hideLabel={true}
+            options={allTagsUtenStjerne}
+            selectedOptions={feedbackTags}
+            onToggleSelected={(option, isSelected) => {
+                handleTagToggle(option, isSelected)
+            }}
+        />
     )
 }
