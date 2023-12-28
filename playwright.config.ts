12158e870500b3978d98f1b01fe02d5a99b458e3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-28 10:30:54 +0100

    Playwright (#297)

---
+++ playwright.config.ts
@@ -0,0 +1,76 @@
+import { defineConfig, devices } from '@playwright/test'
+import { PlaywrightTestConfig } from 'playwright/types/test'
+
+const PORT = process.env.PORT || 3000
+
+type OptionsType = { baseURL: string; timeout: number; server: PlaywrightTestConfig['webServer'] }
+const opts: OptionsType =
+    process.env.CI || process.env.FAST
+        ? // Github Actions runs server in a services image
+          {
+              baseURL: `http://localhost:${PORT}`,
+              timeout: 30 * 1000,
+              server: {
+                  command: 'MOCK_BACKEND=true npm run start',
+                  url: `http://localhost:${PORT}`,
+                  timeout: 120 * 1000,
+                  reuseExistingServer: !process.env.CI,
+              },
+          }
+        : // Local dev server
+          {
+              baseURL: `http://localhost:${PORT}`,
+              timeout: 60 * 1000,
+              server: {
+                  command: 'npm run dev',
+                  url: `http://localhost:${PORT}`,
+                  timeout: 120 * 1000,
+                  reuseExistingServer: !process.env.CI,
+                  stderr: 'pipe',
+                  stdout: 'pipe',
+              },
+          }
+
+export default defineConfig({
+    testDir: './e2e',
+    expect: {
+        timeout: 10 * 1000,
+    },
+    timeout: opts.timeout,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'html',
+    use: {
+        baseURL: opts.baseURL,
+        trace: 'on-first-retry',
+    },
+    webServer: opts.server,
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'] },
+        },
+        {
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 12'] },
+        },
+        ...(!process.env.CI
+            ? [
+                  {
+                      name: 'firefox',
+                      use: { ...devices['Desktop Firefox'] },
+                  },
+                  {
+                      name: 'webkit',
+                      use: { ...devices['Desktop Safari'] },
+                  },
+              ]
+            : []),
+    ],
+})
