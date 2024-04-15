6d0f7c25665353916aff00207d0f3caec3bca59e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-15 09:38:14 +0200

    ikke prosseser

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import React, { useState } from 'react'
 import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { InternalHeader } from '@navikt/ds-react'
-import Image from 'next/image'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     const [queryClient] = useState(
@@ -28,32 +27,16 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
                 <title>Flexjar</title>
                 <meta name="robots" content="noindex" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/manifest.json" />
             </Head>
             <QueryClientProvider client={queryClient}>
                 <InternalHeader>
                     <InternalHeader.Title as="h1">
                         Flexjar
-                        <Image
-                            className="inline"
-                            src="/static/flexjar.png"
-                            alt="flexjarlogo"
-                            width={100}
-                            height={100}
-                        />
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img className="inline" src="/static/flexjar.png" alt="flexjarlogo" width={100} height={100} />
                     </InternalHeader.Title>
                 </InternalHeader>
                 <div id="root" className="mx-auto p-4 pb-32">
