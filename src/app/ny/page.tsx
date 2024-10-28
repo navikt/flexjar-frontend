e1edc790bcdff0c21f80ebc55b33af3c13645784
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-10-28 07:51:42 +0100

    Fjern flexjar i flexjar (#822)

--- src/app/ny/page.tsx
+++ src/app/ny/page.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement, Suspense } from 'react'
 import { Skeleton } from '@navikt/ds-react'
 
 import { FeedbackTabell } from '../../components/FeedbackTabell'
-import { Tilbakemelding } from '../../components/Tilbakemelding'
 
 function Page(): ReactElement {
     return (
@@ -10,7 +9,6 @@ function Page(): ReactElement {
             <Suspense fallback={<Skeleton variant="rounded" height="100%" width="100%" className="p-4" />}>
                 <FeedbackTabell />
             </Suspense>
-            {process.env.MOCK_BACKEND !== 'true' && <Tilbakemelding />}
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
 import { initialProps } from '../initialprops/initialProps'
-import { Tilbakemelding } from '../components/Tilbakemelding'
 import { FeedbackTabell } from '../components/FeedbackTabell'
-import { isMockBackend } from '../utils/environment'
 
 const Index = (): JSX.Element => {
     return (
         <>
             <FeedbackTabell />
-            {!isMockBackend() && <Tilbakemelding />}
         </>
     )
 }
